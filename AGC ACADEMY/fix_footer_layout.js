4705fdd3f37fb5b7de1c057da3a4ef62dd655a77
Joan Chepkoech <joan92311@gmail.com>
2026-03-14 09:29:26 +0300

    minor updates

--- AGC ACADEMY/fix_footer_layout.js
+++ AGC ACADEMY/fix_footer_layout.js
@@ -1,8 +1,8 @@
 const fs=require('fs');
 const path='index.html';
 let text=fs.readFileSync(path,'utf8');
-const old='      <div class="footer__brand">\r\n        <img src="images/logo.svg" alt="AGC Academy logo" class="footer__logo" />\r\n        <div>\r\n          <p class="footer__title">AGC ACADEMY</p>\r\n          <p class="footer__motto">In God We Rise</p>\r\n        </div>\r\n      </div>      <img src="images/agc logo.jpeg" alt="AGC Academy logo" class="footer__logo footer__logo--medium" aria-hidden="true" />\r\n';
-const replacement='      <div class="footer__brand">\r\n        <img src="images/logo.svg" alt="AGC Academy logo" class="footer__logo" />\r\n        <div>\r\n          <p class="footer__title">AGC ACADEMY</p>\r\n          <p class="footer__motto">In God We Rise</p>\r\n        </div>\r\n      </div>\r\n      <img src="images/agc logo.jpeg" alt="AGC Academy logo" class="footer__logo footer__logo--medium" aria-hidden="true" />\r\n';
+const old='      <div class="footer__brand">\r\n        <img src="images/agc logo.jpeg" alt="AGC Academy logo" class="footer__logo" />\r\n        <div>\r\n          <p class="footer__title">AGC ACADEMY</p>\r\n          <p class="footer__motto">In God We Rise</p>\r\n        </div>\r\n      </div>      <img src="images/agc logo.jpeg" alt="AGC Academy logo" class="footer__logo footer__logo--medium" aria-hidden="true" />\r\n';
+const replacement='      <div class="footer__brand">\r\n        <img src="images/agc logo.jpeg" alt="AGC Academy logo" class="footer__logo" />\r\n        <div>\r\n          <p class="footer__title">AGC ACADEMY</p>\r\n          <p class="footer__motto">In God We Rise</p>\r\n        </div>\r\n      </div>\r\n      <img src="images/agc logo.jpeg" alt="AGC Academy logo" class="footer__logo footer__logo--medium" aria-hidden="true" />\r\n';
 if(!text.includes(old)) throw new Error('old block missing');
 text=text.replace(old,replacement);
 fs.writeFileSync(path,text,'utf8');
